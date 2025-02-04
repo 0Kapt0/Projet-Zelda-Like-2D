91261e64e420f87be9ad2208c907e029ea902433
Aurèle Godin <aurelegodin98@outlook.fr>
2025-02-04 17:31:50 +0100

    Update Game.cpp

--- Projet Zelda-Like 2D/src/Game.cpp
+++ Projet Zelda-Like 2D/src/Game.cpp
@@ -5,7 +5,7 @@ using namespace sf;
 using namespace std;
 
 Game::Game()
-    : window(VideoMode(1200, 900), "projet zelda 2D"),
+    : window(VideoMode(1200, 900), "Zeldouille", Style::None),
     player(),
     pauseMenu(window) {
     window.setFramerateLimit(60);
@@ -20,7 +20,9 @@ void Game::run() {
             if (event.type == Event::Closed) {
                 window.close();
             }
-
+            if (event.key.code == Keyboard::R) {
+                window.close();
+            }
             if (event.type == Event::KeyPressed && event.key.code == Keyboard::Escape) {
                 isPaused = !isPaused;
             }
